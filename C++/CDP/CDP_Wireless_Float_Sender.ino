6adb993c98439466517a2e9639ededbb707e43b4
KalebNails <102830532+KalebNails@users.noreply.github.com>
2022-11-11 12:37:54 -0500

    Update CDP_Wireless_Float_Sender.ino
    Had an error in its initial definition of its variable types. The first version was an older version of the code without the fix.

--- C++/CDP/CDP_Wireless_Float_Sender.ino
+++ C++/CDP/CDP_Wireless_Float_Sender.ino
@@ -1,6 +1,6 @@
 // Kaleb Nails
 // Created: 9/23/2022
-// Modified: 10/25/2022
+// Modified: 11/11/2022
 // original name: Testrawsender
 //
 // Purpose: To send 1 floating point number to another hellteck wirelessly using CDP.
@@ -16,18 +16,24 @@
 #include <arduino-timer.h>
 #include <string>
 
+#ifdef SERIAL_PORT_USBVIRTUAL
+#define Serial SERIAL_PORT_USBVIRTUAL
+#endif
+
+
 MamaDuck duck;
 
 
-const int INTERVAL_MS = 10000;
+DuckDisplay* display = NULL;
+
+const int INTERVAL_MS = 1000;
 int counter = 1;
 bool sentOk = false;
 char Buffer[100];
-#define N_FLOATS 1
-uint8_t myChars[N_FLOATS];
-float myFloats[4*N_FLOATS];
+#define N_FLOATS 2
+uint8_t myChars[4*N_FLOATS];
+float myFloats[N_FLOATS];
 
-auto timer = timer_create_default();
 
 // LORA RF CONFIG
 #define LORA_FREQ 915.0 // Frequency Range. Set for US Region 915.0Mhz
@@ -38,13 +44,14 @@ auto timer = timer_create_default();
 #define LORA_DIO1_PIN -1 // unused
 #define LORA_RST_PIN 14
 
+auto timer = timer_create_default();
 
 void setup() {
   // We are using a hardcoded device id here, but it should be retrieved or
   // given during the device provisioning then converted to a byte vector to
   // setup the duck NOTE: The Device ID must be exactly 8 bytes otherwise it
   // will get rejected
-  std::string deviceId("MAMA0001");
+  std::string deviceId("MAMA0002");
   std::vector<byte> devId;
   devId.insert(devId.end(), deviceId.begin(), deviceId.end());
   //duck.setupWithDefaults(devId);
@@ -66,7 +73,9 @@ void setup() {
 
   
    timer.every(INTERVAL_MS, runSensor);
-   myFloats[0] = 1.2 + float(counter);
+   myFloats[0] = 2.6;
+   myFloats[1] = 1.8;
+   
    
    
 }
@@ -79,7 +88,7 @@ void setup() {
 
   
   //memcpy(myChars, myFloats, sizeof(myFloats));
-  
+  //myFloats[0] = (float) millis();
   timer.tick();
   
   // Use the default run(). The Mama duck is designed to also forward data it
@@ -92,33 +101,39 @@ void setup() {
 bool runSensor(void *) {
   bool result;
   const byte* buffer;
+  
+  memcpy(myChars, myFloats, sizeof(myFloats));
+
   /*
-  String message = String("Counter:") + String(counter);
+  String message =  String(myChars);
   int length = message.length();
   Serial.print("[MAMA] sensor data: ");
   Serial.println(message);
   buffer = (byte*) message.c_str(); 
 */
 
-  memcpy(myChars, myFloats, sizeof(myFloats));
-
-  result = sendData(myChars, sizeof(myChars));
+  //memcpy(myChars, myFloats, sizeof(myFloats));
+  //result = sendData(myChars, sizeof(myChars));
+  int length = sizeof(myChars);
+  result = sendData(myChars, length);
   if (result) {
     Serial.println("[MAMA] runSensor ok.");
   } else {
     Serial.println("[MAMA] runSensor failed.");
   }
+  
   return result;
 }
 
-bool sendData(const byte* buffer, int length) {
+bool sendData(const byte* myChars, int length) {
   bool sentOk = false;
   
   // Send Data can either take a byte buffer (unsigned char) or a vector
-  int err = duck.sendData(topics::status, myChars, sizeof(myChars));
+  int err = duck.sendData(topics::status, myChars, length);//myChars, sizeof(myChars));
   if (err == DUCK_ERR_NONE) {
     counter++;
-    myFloats[0] = 1.2 + float(counter);
+    myFloats[0]++;
+    //myFloats[0] = 1.2 + float(counter);
     sentOk = true;
   }
   if (!sentOk) {
@@ -126,15 +141,3 @@ bool sendData(const byte* buffer, int length) {
   }
   return sentOk;
 }
-
-
-/*
-bool runSensor(void*) {
-
-
-  
-  memcpy(myChars, myFloats, sizeof(myFloats));
-  duck.sendData(topics::status, myChars, sizeof(myChars));
-  
-
-}*/
