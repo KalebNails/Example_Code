4b15e7f69d7be66f5a94ee5f104fc04d1983f407
KalebNails <102830532+KalebNails@users.noreply.github.com>
2023-01-20 14:21:25 -0500

    Update ethernet_USB_sender.py
    forgot to include encode on the first upload. Also added it into a json compatible format.

--- Python/Serial_Ports/ethernet_USB_sender.py
+++ Python/Serial_Ports/ethernet_USB_sender.py
@@ -1,14 +1,19 @@
 #Author: Kaleb Nails
 #Created: 1/10/2023
+#modified: 1/20/2023
 #Purpose: This is running on a rasberry PI that has its eithernet plug into another computer. 
 # a great instruction video about this process is: https://www.youtube.com/watch?v=S7Yle8clJ30
 
 import socket
-msgtosend = '[value:103,value2: 1.34]'
+#msgtosend = '[value:103,value2: 1.34]'
+time_example = 1.4
+msgtosend = '{"Sensor": "PurpleAir0", "value":"%s"}' %(time_example)
 
 #This is hardcoded per computer
 ServerAddress = ('169.254.26.44',2222)
 bufferSize = 1024
+
+bytestosend = msgtosend.encode('utf-8')
 
 #This creates the default socket settings and sends the buffer
 UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
