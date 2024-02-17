b7643b0faa816bc90dd0eef20fc17c106a17a9e5
KalebNails <102830532+KalebNails@users.noreply.github.com>
2024-02-16 20:58:17 -0500

    Update linux_discord.py
    this lets you upload files to discord

--- Python/discord_bots/linux_discord.py
+++ Python/discord_bots/linux_discord.py
@@ -105,7 +105,7 @@ async def on_message(message):
                     if len(date_cell) == 0:
                         python_script_path = 'discord_meteogram.py'
                         print(python_script_path)
-                        await message.channel.send("Generating from demo linux 1 date")
+                        await message.channel.send("Generating todays data from the linux machine")
                         subprocess.run(['python',python_script_path]) #######################
                         await message.channel.send( file=discord.File('meteogram_image.png'))
 
@@ -121,7 +121,7 @@ async def on_message(message):
                                 python_script_path = f"{date_cell[0]['year']}_{str(date_cell[0]['month']).zfill(2)}_{str(date_cell[0]['day']).zfill(2)}"
 
                                 print(python_script_path)
-                                await message.channel.send(f"Date exists. Generating demo linux {file_path}")
+                                await message.channel.send(f"Date exists. Generating linux from {file_path}")
                                 #subprocess.run(['python',python_script_path]) #######################
                                 subprocess.run(['python','discord_meteogram.py',python_script_path]) #######################
 
@@ -163,7 +163,7 @@ async def on_message(message):
                             print(f"Date exist between {date_start} {date_end}.")
 
                             print(python_script_path)
-                            await message.channel.send(f"Date exist between {date_start} {date_end}. Generating demo linux")
+                            await message.channel.send(f"Date exist between {date_start} {date_end}. Generating from linux")
                             #subprocess.run(['python',python_script_path]) #######################
                             subprocess.run(['python','discord_meteogram.py',date_start,date_end])
                             await message.channel.send( file=discord.File('meteogram_image.png'))
@@ -185,9 +185,76 @@ async def on_message(message):
                 await message.channel.send(f"An error has occurred: {str(e)}")
 
 #HELP COMMAND
-        if message.content.lower() == 'help':#'ip?':
+        if 'help' in message.content.lower():#'ip?':
             try:
-                await message.channel.send("LIST OF COMMANDS (case insensitive):\nmeto: produces a meteogram graph of today\nifconfig: will print out the contents of ifconfig from the Linux terminal\ntraceroute: will traceroute and give the bubble IP")
+                await message.channel.send(
+                    "LIST OF COMMANDS (case insensitive):\n"
+                    "meto: produces a meteogram graph of today\n"
+                    "meto YYYY_MM_DD: will return the data at a specific date\n"
+                    "meto YYYY_MM_DD YYYY_MM_DD: creates a graph of the fusion between all of the dates"
+                    "ifconfig: will print out the contents of ifconfig from the Linux terminal\n"
+                    "traceroute: will traceroute and give the bubble IP\n"
+                )
+            except Exception as e:
+                await message.channel.send(f"An error has occurred: {str(e)}")
+
+
+
+        #THIS IS TO UPLOAD DATA
+        if 'upload' in message.content.lower():
+            try:
+                print('its true')
+                python_script_path = 'discord_meteogram.py'
+
+                #argnum dictates how many arguements have as an input and sort the dates in order
+                date_cell = contains_date(message.content.lower())
+                print(f"the date cell is outside function {date_cell}")
+
+                #sort the data by date
+                print(f"the unsorted date_cell: \n {date_cell}")
+                date_cell = sorted(date_cell, key=lambda x: (x['year'], x['month'], x['day']))
+                print(f"the sorted date_cell: \n {date_cell}")
+
+                #checks for the propper amounts of dates
+                if  0<= len(date_cell) <= 1:
+
+                    #THINK I CAN DELETE THE NEXT 3 lines
+                    #for i in date_cell:
+                        #python_script_path += f" {i['year']}_{str(i['month']).zfill(2)}_{str(i['day']).zfill(2)}"
+                        #print(python_script_path)
+
+                    if len(date_cell) == 0:
+                        python_script_path = 'discord_meteogram.py'
+                        print(python_script_path)
+                        await message.channel.send("pulling todays data from the linux machine")
+                        await message.channel.send( file=discord.File('current_weather_data_logfile.csv'))
+
+                    #double check file exists
+                    elif len(date_cell)==1:
+                        print('using provided date')
+                        file_path = f"{directory_location}{date_cell[0]['year']}_{str(date_cell[0]['month']).zfill(2)}_{str(date_cell[0]['day']).zfill(2)}_weather_station_data.csv"
+
+                        if os.path.exists(file_path):
+                                print("File exists.")
+
+                                #python_script_path = f"discord_meteogram.py {date_cell[0]['year']}_{str(date_cell[0]['month']).zfill(2)}_{str(date_cell[0]['day']).zfill(2)}"
+                                python_script_path = f"{date_cell[0]['year']}_{str(date_cell[0]['month']).zfill(2)}_{str(date_cell[0]['day']).zfill(2)}"
+
+                                print(python_script_path)
+                                await message.channel.send(f"Date exists. uploading {file_path}")
+                                await message.channel.send( file=discord.File(file_path))
+                        else:
+                            print(f"File {file_path} does not exist. defaulting to current date")
+                            await message.channel.send(f" {file_path} {date_end} does not exist ")
+
+
+                else:
+                    error_message = '''invalid amount of dates sent please enter following the example inputs below:
+                        current date: upload
+                        specific date: upload 2024_08_01'''
+
+                    print(error_message)
+                    await message.channel.send(error_message)
 
             except Exception as e:
                 await message.channel.send(f"An error has occurred: {str(e)}")
@@ -197,4 +264,13 @@ async def on_message(message):
 
 
 
-client.run('YOUR_AUTH')
+
+
+
+
+
+            except Exception as e:
+                await message.channel.send(f"An error has occurred: {str(e)}")
+
+
+client.run('YOUR AUTH')
